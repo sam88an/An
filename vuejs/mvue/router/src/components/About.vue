1c1cf109625bc764609c4834b35fff3692d6df04
sam88an@gmail.com <sam88an@gmail.com>
2020-08-19 23:14:31 +0700

    first commit

---
+++ vuejs/mvue/router/src/components/About.vue
@@ -0,0 +1,23 @@
+ <template>
+  <div class="about">
+    
+    <h2>This is About Page</h2>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
---
+++ vuejs/mvue/router/src/components/Contact.vue
@@ -0,0 +1,23 @@
+ <template>
+  <div class="contact">
+    
+    <h2>This is our Contact Page</h2>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
